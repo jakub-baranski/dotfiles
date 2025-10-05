9b24018005788cd28f04fc64a2b10f58e3fd2522
jakub-baranski <jak.bar92@gmail.com>
2025-10-05 02:17:56 +0200

    change dashboard layout

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -88,6 +88,27 @@ return {
       local header_lines = headers[day] or headers.Monday
       opts.dashboard.preset.header = table.concat(header_lines, "\n")
 
+      opts.dashboard.sections = {
+        { section = "header" },
+        { icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
+        { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+        { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+        {
+          icon = " ",
+          title = "Git Status",
+          section = "terminal",
+          enabled = function()
+            return Snacks.git.get_root() ~= nil
+          end,
+          cmd = "git status --short --branch --renames",
+          height = 5,
+          padding = 1,
+          ttl = 5 * 60,
+          indent = 3,
+        },
+        { section = "startup" },
+      }
+
       return opts
     end,
   },
